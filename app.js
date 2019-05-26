89f1afcfa8ce43b9d392940c772dc551f67657e1
MeanManMachineMan <vidur.p11@gmail.com>
2019-05-26 20:05:46 +0530

    Week 1

---
+++ app.js
@@ -0,0 +1,29 @@
+(function(){
+'use strict'
+
+angular.module('mod1',[])
+	.controller('LunchCheckController',lccontrol);
+lccontrol.$inject = ['$scope'];
+function lccontrol($scope){
+	$scope.list = "";
+	$scope.check = function(){
+		if($scope.list == ""){
+			$scope.output = "Please enter data first!"
+		}
+		else{
+			var words = $scope.list.split(',');
+			var a = words.length;
+			words.forEach(function(item){ 
+				if(item =="")
+					a -= 1
+				});
+			if(a <= 3)
+				$scope.output = "Enjoy!"
+			else
+				$scope.output = "Too much!"
+		}
+	}
+};
+
+
+})();
